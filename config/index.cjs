65b39e023b7be138825b1f6f073c7ab5b5e163c8
hasaiji33 <115968048+hasaiji33@users.noreply.github.com>
2022-10-17 12:48:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,16 +27,12 @@ const USER_CONFIG = {
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '阿倩宝贝', year: '2000', date: '11-10',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '阿倩宝贝', year: '2000', date: '11-10',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2016', date: '09-01',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -53,7 +49,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '爱你的场猪',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
     }
