509e0f03a13678f5ad9e4f92434b1768fd4c05a7
hasaiji33 <115968048+hasaiji33@users.noreply.github.com>
2022-10-17 12:34:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,28 +8,28 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx74892d45c4ea2285',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '1fd1e4df80f026840c89db5a4e8be803',
 
   PROVINCE: '广东',
-  CITY: '惠州',
+  CITY: '顺德',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '阿倩宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'og3Qv5h5YMc8zcVENIQ8wK5Ijk9E',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'F5gxcRwwNEarqW4xl3xsnVRZns4g687200meKxZ3HTc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '阿倩宝贝', year: '2000', date: '11-10',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
@@ -42,9 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2016-12-24' },
       ],
     },
   ],
